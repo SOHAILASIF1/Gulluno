b910280c904cd8caa66486db4bb11b2ff6373c77
SOHAILASIF1 <alyana998877@gmail.com>
2025-08-04 20:06:31 +0500

    hgjh

--- client/src/commen/index.js
+++ client/src/commen/index.js
@@ -1,114 +1,102 @@
-const backendDomain='http://localhost:4000'
+const backendDomain = process.env.REACT_APP_API_BASE_URL;
 
-const SummaryApi={
-    signup:{
-        url:`${backendDomain}/api/user/signup`,
-        method:'post'
+const SummaryApi = {
+    signup: {
+        url: `${backendDomain}/api/user/signup`,
+        method: 'post'
     },
-    login:{
-        url:`${backendDomain}/api/user/login`,
-        method:'post'
+    login: {
+        url: `${backendDomain}/api/user/login`,
+        method: 'post'
     },
-    userDetail:{
-        url:`${backendDomain}/api/user/details`,
-        method:'get'
+    userDetail: {
+        url: `${backendDomain}/api/user/details`,
+        method: 'get'
     },
-    logout:{
-        url:`${backendDomain}/api/user/logout`,
-        method:'get'
+    logout: {
+        url: `${backendDomain}/api/user/logout`,
+        method: 'get'
     },
-    allUser:{
-        url:`${backendDomain}/api/user/allUser`,
-        method:'get'
+    allUser: {
+        url: `${backendDomain}/api/user/allUser`,
+        method: 'get'
     },
-    updateUser:{
-        url:`${backendDomain}/api/user/updateUser`,
-        method:'post'
+    updateUser: {
+        url: `${backendDomain}/api/user/updateUser`,
+        method: 'post'
     },
-    uploadProduct:{
-        url:`${backendDomain}/api/product/uploadProduct`,
-        method:"post"
+    uploadProduct: {
+        url: `${backendDomain}/api/product/uploadProduct`,
+        method: "post"
     },
-    updateProduct:{
-        url:`${backendDomain}/api/product/updateProduct`,
-        method:"post"
+    updateProduct: {
+        url: `${backendDomain}/api/product/updateProduct`,
+        method: "post"
     },
-    getAllProduct:{
-        url:`${backendDomain}/api/product/allProduct`,
-        method:"get"
+    getAllProduct: {
+        url: `${backendDomain}/api/product/allProduct`,
+        method: "get"
     },
-    deleteProduct:{
-        url: (id)=>`${backendDomain}/api/product/deleteProduct/${id}`,
-        method:"delete"
+    deleteProduct: {
+        url: (id) => `${backendDomain}/api/product/deleteProduct/${id}`,
+        method: "delete"
     },
-    categoryWiseProduct:{
-        url:`${backendDomain}/api/product/categoryWiseProduct`,
-        method:"post"
+    categoryWiseProduct: {
+        url: `${backendDomain}/api/product/categoryWiseProduct`,
+        method: "post"
     },
-    getCategory:{
-        url:`${backendDomain}/api/product/getCategory`,
-        method:"get"
-
+    getCategory: {
+        url: `${backendDomain}/api/product/getCategory`,
+        method: "get"
     },
-    getProductDetail:{
-        url:`${backendDomain}/api/product/getProductDetails`,
-        method:"post"
-
+    getProductDetail: {
+        url: `${backendDomain}/api/product/getProductDetails`,
+        method: "post"
     },
-    addToCart:{
-        url:`${backendDomain}/api/user/addToCart`,
-        method:"post"
-
+    addToCart: {
+        url: `${backendDomain}/api/user/addToCart`,
+        method: "post"
     },
-    countCart:{
-        url:`${backendDomain}/api/user/countCart`,
-        method:"get"
-        
+    countCart: {
+        url: `${backendDomain}/api/user/countCart`,
+        method: "get"
     },
-    allCartProduct:{
-        url:`${backendDomain}/api/user/getCartProduct`,
-        method:"get"
-        
+    allCartProduct: {
+        url: `${backendDomain}/api/user/getCartProduct`,
+        method: "get"
     },
-    updateCart:{
-        url:`${backendDomain}/api/user/updateCart`,
-        method:"post"
-        
+    updateCart: {
+        url: `${backendDomain}/api/user/updateCart`,
+        method: "post"
     },
-    deleteCart:{
-        url:`${backendDomain}/api/user/deleteCart`,
-        method:"post"
-        
+    deleteCart: {
+        url: `${backendDomain}/api/user/deleteCart`,
+        method: "post"
     },
-    search:{
-        url:`${backendDomain}/api/product/search`,
-        method:"get"
-        
+    search: {
+        url: `${backendDomain}/api/product/search`,
+        method: "get"
     },
-    createOrder:{
-        url:`${backendDomain}/api/order/create-order`,
-        method:"post"
-        
+    createOrder: {
+        url: `${backendDomain}/api/order/create-order`,
+        method: "post"
     },
     getOrder: {
         url: `${backendDomain}/api/order/getOrder`,
         method: "get"
-      },
-      updateOrderStatus: {
-        url: `${backendDomain}/api/order/update-order-status`,
-        method: "PUT",
-      },
-
-
-      deleteOrder:{
-        url: (orderId)=>`${backendDomain}/api/order/deleteOrder/${orderId}`,
-        method:"delete"
     },
-    sendEmail:{
-        url:`${backendDomain}/api/user/contactUs`,
-        method:"post"
+    updateOrderStatus: {
+        url: `${backendDomain}/api/order/update-order-status`,
+        method: "PUT"
+    },
+    deleteOrder: {
+        url: (orderId) => `${backendDomain}/api/order/deleteOrder/${orderId}`,
+        method: "delete"
+    },
+    sendEmail: {
+        url: `${backendDomain}/api/user/contactUs`,
+        method: "post"
     }
-      
-    
-}
-export default SummaryApi
+};
+
+export default SummaryApi;
