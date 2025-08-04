bb47a0c65bfc32a3b14da394d5a0304dbb4c7b04
SOHAILASIF1 <alyana998877@gmail.com>
2025-08-04 20:21:16 +0500

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,13 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173", // Frontend URL
-  credentials: true, // Allow cookies
+  origin: [
+    "http://localhost:5173",
+    "https://gulluno-u8tq.vercel.app"
+  ],
+  credentials: true,
 }));
+
 
 
 app.use('/api/user',userRouter)
