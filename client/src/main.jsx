5106a545463a8ce11eb95c0d135288c2c217b591
SOHAILASIF1 <alyana998877@gmail.com>
2025-08-08 09:02:55 +0500

    hjdjks

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import React  from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
  
     <Provider store={store}>
     <App />
+    <SpeedInsights/>
     </Provider>
  
 )
