90f9145ef02b633c30295ca6328ed46f5ffed9fc
SOHAILASIF1 <alyana998877@gmail.com>
2025-08-08 14:28:48 +0500

    ghjska

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -81,10 +81,11 @@ function Header() {
     <img 
       src={logo} 
       alt="Logo" 
-      className="h-12 w-20 object-contain md:h-14" 
+      className="h-16 w-auto object-contain md:h-20" 
     />
   </Link>
 </div>
+
 
         </div>
 
